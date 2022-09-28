3fd4e4e39f9b1ab197c81766d072dbc614dd09f7
aidan_kao <mic85214@gmail.com>
2022-09-28 23:12:47 +0800

    20220928: add avatar & modified about.md

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220928.224019';
+const cacheName = 'chirpy-20220928.231058';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
