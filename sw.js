3d659931641521d30a713802bb87f9633e931ef4
aidan_kao <mic85214@gmail.com>
2022-09-29 23:55:53 +0800

    20220929: add 5 posts

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220928.231058';
+const cacheName = 'chirpy-20220929.235419';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
